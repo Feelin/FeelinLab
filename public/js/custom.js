287b506fe21c328e0af9db099527f2b8bee36ab2
Feelin <williamgeeker@gmail.com>
2014-05-09 14:35:54 +0800

    reset

--- public/js/custom.js
+++ public/js/custom.js
@@ -30,4 +30,39 @@ var resizeHeight = function (){
         $(this).css("display","none");
         $(".bigpic").css("display","none");
     });
+///////////////////////////loadimg///////////////////
+    $(".cover").on("click",function(){       
+        var size = $(this).data("size");
+        var local = $(this).data("local");
+        var photos = ["","","",""];
+        var j = 1;
+        var k = 0;    
+        while(j<size){    
+            photos[k%3] += '<img class="photo" src="../public/photo/'+local+'/'+j+'.jpg">';
+            j++;
+            k++;
+        }  
+
+        $(".col-6").remove();
+        for(l=0;l<3;l++){
+            $(".col-4").eq(l).append(photos[l]);
+        }   
+
+        $(".photo").on("click",function(){
+            var src = $(this).attr("src");
+            var width = parseInt($(this).css("width"));
+            var height = parseInt($(this).css("height"));
+            $(".lightbox").css("display","block");
+            $(".bigpic").attr("src",src).css("display","block");
+            if(height>width){
+                $(".bigpic").addClass("colimg");
+            }
+            else{
+                $(".bigpic").removeClass("colimg");
+            }
+        }); 
+
+    });
+  
+
 });
